7dcfa2c4223e5af609654cdecde982fe588fbe87
Diego de Massy <ddemassy@gmail.com>
2022-06-05 09:53:36 +0200

    Technologies + timeline (svg...)

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,48 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've work with a range a technologies in Sed natus sapiente dolore quos, quas voluptate aptent. Incidunt, ipsum! Cillum elementum dolor faucibus, amet erat optio. Id labore pellentesque porttitor erat, repellendus 
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node, Drupal <br/>
+            and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            tools like Justinmind <br/>
+            and photoshop
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
